2917049162acd797f450ad8b31ffdb8edbdc9f80
sheldonhh <xphuangsg@gmail.com>
2018-09-10 10:44:32 +0800

    change firebaseWebApp

--- src/main.js
+++ src/main.js
@@ -19,11 +19,12 @@ new Vue({
   render: h => h(App),
   created () {
     firebase.initializeApp({
-      apiKey: 'AIzaSyA1c1UMOXzujqU3QFMiS7g0pjjwVR-CT-M',
-      authDomain: 'youtube-devmeetup.firebaseapp.com',
-      databaseURL: 'https://youtube-devmeetup.firebaseio.com',
-      projectId: 'youtube-devmeetup',
-      storageBucket: ''
+      apiKey: 'AIzaSyAvV69wMe_5Z5r9nL_Lakd_AWF8wirknqc',
+      authDomain: 'first-aae60.firebaseapp.com',
+      databaseURL: 'https://first-aae60.firebaseio.com',
+      projectId: 'first-aae60',
+      storageBucket: 'first-aae60.appspot.com',
+      messagingSenderId: '568802721528'
     })
   }
 })
